cada2ca0501cfef0b4fb65f7e3049c9150191e61
Cyteon <cyteon>
2024-07-24 10:13:17 +0000

    make client id variable in handleCommands and env var

--- src/functions/handelCommands.js
+++ src/functions/handelCommands.js
@@ -17,22 +17,23 @@
 */
 
 const { REST } = require("@discordjs/rest");
-const { Routes } = require('discord-api-types/v9');
+const { Routes } = require("discord-api-types/v9");
 const { SlashCommandBuilder } = require("discord.js");
-const fs = require('fs');
+const fs = require("fs");
 
-const clientId = '1249288048894808074'; 
-const guildId = '1233502082686779412'; 
+const clientId = process.env.client_id;
 
 module.exports = (client) => {
     client.handleCommands = async (commandFolders, path) => {
         client.commandArray = [];
         for (folder of commandFolders) {
-            const commandFiles = fs.readdirSync(`${path}/${folder}`).filter(file => file.endsWith('.js'));
+            const commandFiles = fs
+                .readdirSync(`${path}/${folder}`)
+                .filter((file) => file.endsWith(".js"));
             for (const file of commandFiles) {
                 const command = require(`../commands/${folder}/${file}`);
                 client.commands.set(command.data.name, command);
-                
+
                 if (command.data instanceof SlashCommandBuilder) {
                     client.commandArray.push(command.data.toJSON());
                 } else {
@@ -43,20 +44,18 @@ module.exports = (client) => {
         }
 
         const rest = new REST({
-            version: '9'
+            version: "9",
         }).setToken(process.env.token);
 
         (async () => {
             try {
-                console.log('Started refreshing application (/) commands.');
+                console.log("Started refreshing application (/) commands.");
 
-                await rest.put(
-                    Routes.applicationCommands(clientId), {
-                        body: client.commandArray
-                    },
-                );
+                await rest.put(Routes.applicationCommands(clientId), {
+                    body: client.commandArray,
+                });
 
-                console.log('Successfully reloaded application (/) commands.');
+                console.log("Successfully reloaded application (/) commands.");
             } catch (error) {
                 console.error(error);
             }
